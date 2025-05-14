d8e355921e1ba6a5f3b61796b26c012cdb21c090
ShanhinAndrey <a.a.shanhin@student.khai.edu>
2025-05-14 09:08:05 +0300

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "array_utils.h"
+#include "matrix_utils.h"
+#include "sort_utils.h"
+
+using namespace std;
+int main()
+{
+    int choice;
+
+    do
+    {
+        cout << "\nSelect task:\n"
+            << "1. Array87\n"
+            << "2. Matrix35\n"
+            << "3. Sort12\n"
+            << "0. Выход\n"
+            << "Your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            array_87();
+            break;
+        case 2:
+            matrix_35();
+            break;
+        case 3:
+            sort_12();
+            break;
+        case 0:
+            cout << "Exit the program.\n";
+            break;
+        default:
+            cout << "Wrong choice! Try again.\n";
+        }
+    } while (choice != 0);
+
+    return 0;
+}
